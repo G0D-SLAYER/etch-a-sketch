2fb6ea66c6688e8431df14cab14669efb7add2bc
G0D-SLAYER <159141717+G0D-SLAYER@users.noreply.github.com>
2025-01-11 13:34:58 +0530

    added hover effects on the grid

--- scripts.js
+++ scripts.js
@@ -1,6 +1,13 @@
 const row = 16;
 const column = 16;
 
+function getRandomRGB() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
 const container = document.querySelector("#container");
 let div = null
 
@@ -8,7 +15,7 @@ let containerStyle = window.getComputedStyle(container);
 let width = parseInt(containerStyle.getPropertyValue("width"));
 let gridSide = (width/16) - 2;
 let side = gridSide+"px";
-console.log(width);
+// console.log(width);
 
 for (i = 0; i < row; i++){
     for (j = 0; j < column; j++){
@@ -20,3 +27,16 @@ for (i = 0; i < row; i++){
     }
 }
 
+const grid = document.querySelectorAll(".grid");
+
+grid.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+        const randomColor = getRandomRGB();
+        square.style.backgroundColor = randomColor;
+  
+        let currentOpacity = parseFloat(square.style.opacity);
+        if (currentOpacity < 1) {
+          square.style.opacity = (currentOpacity + 0.1).toString();
+        }
+    });
+})
